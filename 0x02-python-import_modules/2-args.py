44c1c728f438bc8265158a446a0a9e239bbde69a
root <root@Jonathan.localdomain>
2023-03-11 14:38:31 +0300

    Q2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys.argv
+counter = len(argv)
+i = 0
+if counter == 0:
+    print("0 arguments.")
+else:
+    print("{} arguments".format(counter))
+    while counter > 0:
+        print("{} {}".format(i + 1, argv[i]))
+        i = i + 1
