fc0483ef0ecf3beddf28141dd66701d761140d3a
root <root@Jonathan.localdomain>
2023-03-11 13:30:36 +0300

    Q0 more corrections

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    main()
-def main():
 a = 1
 b = 2
-c = add(a, b)
-print("{} + {} = {}".format(a, b, c)
+print("{} + {} = {}".format(a, b, add(a, b)))
