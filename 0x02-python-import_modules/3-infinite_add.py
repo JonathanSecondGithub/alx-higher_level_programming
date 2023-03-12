4d6e2d4e434f10797eb8f5eb61a39c317f3fc1f0
root <root@Jonathan.localdomain>
2023-03-12 14:09:08 +0300

    Q3 more corrections

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,6 @@ if __name__ == "__main__":
     import sys
 counter = len(sys.argv) - 1
 sum = 0
-i = 0
-while counter > 0:
-    sum = sum + int(sys.argv[i + 1])
-    i++
-    counter--
+for i in range(counter)
+    sum += sys.argv[i + 1]
 print("{}".format(sum))
