38230de35ff8641780af2b2876563b291c3b24d7
root <root@Jonathan.localdomain>
2023-03-12 13:47:36 +0300

    Q3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+counter = len(sys.argv) - 1
+sum = 0
+i = 1
+while counter > 0:
+    sum = sum + argv[i]
+    i++
+    counter--
+print("{}".format(sum))
