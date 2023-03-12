2364d6ddc863334e8830cb4f75dc00ab68f3fc84
root <root@Jonathan.localdomain>
2023-03-12 13:23:26 +0300

    Q2 corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,6 @@ elif counter == 1:
 else:
     print("{} arguments:".format(counter))
 while counter > 0:
-    print("{}: {}".format(i + 1, argv[i + 1]))
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
     i = i + 1
     counter = counter - 1
