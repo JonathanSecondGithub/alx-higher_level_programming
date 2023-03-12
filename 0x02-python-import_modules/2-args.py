e27e54fe412e1065f4de0f141075057fd6a1fc14
root <root@Jonathan.localdomain>
2023-03-12 12:44:49 +0300

    Q2 corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,10 @@ counter = len(argv)
 i = 0
 if counter == 0:
     print("0 arguments.")
-else:
+elif counter == 1:
+    print("1 argument:")
+    print("{}: {}".format(counter, argv[counter]))
+else: 
     print("{} arguments:".format(counter))
     while counter > 0:
         print("{}: {}".format(i + 1, argv[i]))
