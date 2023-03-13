cd70227cba77d85c8fec6ff237fff11e95fac6e7
root <root@Jonathan.localdomain>
2023-03-13 13:08:28 +0300

    Q0 corrections

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,6 +3,6 @@ def print_list_integer(my_list=[]):
     counter = len(my_list)
     i = 0
     while counter > 0:
-        print("{}".format(my_list[i]))
+        print("{}".format(int(my_list[i])))
         counter -= 1
         i += 1
