32823156cb3d3857b736d3cb3b6143f1e447df09
root <root@Jonathan.localdomain>
2023-03-15 05:41:45 +0300

    all files

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def print_list_integer(my_list=[]):
-    counter = len(my_list)
-    i = 0
-    while counter > 0:
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
-        counter -= 1
-        i += 1
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+# 2-replace_in_list.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def replace_in_list(my_list, idx, element):
-    list2 = my_list
-    if idx < 0 or idx > len(my_list) - 1:
-        return (my_list)
-    else:
-        list2[idx] = element
-    return (list2)
+    """Replace an element of a list at a specific position."""
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
