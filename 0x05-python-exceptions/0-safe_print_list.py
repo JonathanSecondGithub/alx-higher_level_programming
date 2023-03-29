db58c0a992686b808b1406fff255050cf9d0d146
root <root@Jonathan.localdomain>
2023-03-29 10:36:56 +0300

    fix errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         while i <= x:
-            print(my_list[x])
+            print("{:d}".format(my_list[x]))
             i++     
-    except IndexError:
+    except (IndexError,TypeError):
         pass
