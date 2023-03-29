97cc3daa0ed88290cfcb8a05329f0f510c85e261
root <root@Jonathan.localdomain>
2023-03-29 12:25:22 +0300

    fixed Q2 errors

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except TypeError:
-        print("wrong type")
-
+        return True
+    except ValueError:
+        pass
+        return False
