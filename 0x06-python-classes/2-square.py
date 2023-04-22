921cf2e69e8f24b235284a95c435405a44177f5b
root <root@jonathan-pc.localdomain>
2023-04-22 17:05:53 +0300

    Error detection

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,10 +4,9 @@
 class Square:
     """class Square"""
     def __init__(self, size=0):
-        try:
-            self.size = __size
-        except ValueError as e:
-                    print("size must be >= 0")
-        except TypeError as e:
-            print("size must be an integer")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
