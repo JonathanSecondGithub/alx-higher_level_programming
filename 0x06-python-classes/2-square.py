63406c04b94f08d3f3bc8a7bfc2241944105d56f
root <root@jonathan-pc.localdomain>
2023-04-22 17:07:57 +0300

    Documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,6 +4,8 @@
 class Square:
     """class Square"""
     def __init__(self, size=0):
+       """Args:
+            size (int): square size."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
