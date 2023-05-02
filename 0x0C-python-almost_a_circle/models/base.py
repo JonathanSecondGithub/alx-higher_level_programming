a2f6ea8ae2a7f220fecf86e8bf0cc0f26f363fa0
root <root@jonathan-pc.localdomain>
2023-05-02 13:08:39 +0300

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+"""This is a base shape class"""
 
 class Base():
+    """
+        Represent the base model.
+        Represents the "base" for all other classes in project 0x0C*.
+        Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
+
     __nb_objects = 0
     def __init__(self, id=None):
+        """
+            Initialize a new Base.
+            Args:
+            id (int): The identity of the new Base.
+        """
         if id != None:
             self.id = id
         else:
-            __nb_objects = __nb_objects + 1
-            self.id = __nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
