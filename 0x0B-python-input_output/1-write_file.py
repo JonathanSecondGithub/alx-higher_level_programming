a4d69f93579c7cbcb8a035ede3ae05e35b095b61
root <root@jonathan-pc.localdomain>
2023-05-03 20:41:42 +0300

    Corrections

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Function to write test to a file"""
+"""Defines a file-writing function."""
+
 
 def write_file(filename="", text=""):
-    """Function to write test to a file"""
-    with open(filename , "r") as f:
-        """Open the file"""
-        f.write(test)
-return len(text)_
-        
+    """Write a string to a UTF8 text file.
+        Args:
+            filename (str): The name of the file to write.
+            text (str): The text to write to the file.
+            Returns:
+            The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
