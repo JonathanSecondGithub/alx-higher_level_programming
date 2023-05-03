b24f83f440044489c03239f8e23ef51a3aa39288
root <root@jonathan-pc.localdomain>
2023-05-03 20:36:42 +0300

    Corrections

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Function to write test to a file"""
 
 def write_file(filename="", text=""):
+    """Function to write test to a file"""
     with open(filename , "r") as f:
+        """Open the file"""
         f.write(test)
 return len(text)_
         
