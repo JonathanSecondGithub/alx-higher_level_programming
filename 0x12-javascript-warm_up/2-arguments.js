064ff0e93453ed0184dc6a0c29b017b9e911f938
Sir Jonathan Kariuki <jonamkariuki@gmail.com>
2023-06-12 21:46:38 +0300

    Q2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 //Write a script that prints a message depending of the number of arguments passed
 
-let args = process.argv.length  - 1;
+let args = process.argv.length  - 2;
 
 if (args === 0){
 	console.log('No argument');
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+//Write a script that prints the first argument passed to it
+
+let 
