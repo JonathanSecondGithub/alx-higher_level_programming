5a9c7d6e5fcb81d1447bff35c3b5b3a11fa64c42
Sir Jonathan Kariuki <jonamkariuki@gmail.com>
2023-06-14 23:24:11 +0300

    Q3 corrections

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@
 //Write a script that prints the first argument passed to it
 const arg0 = process.argv[2];
 
-if (len(arg0) === 0) {
+if (not arg0) {
 	console.log('No argument');
 } else {
 	console.log(arg0);
