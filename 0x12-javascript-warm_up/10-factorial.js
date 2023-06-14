5b7bdd5b05f344577b0eb86ac08d138ef2e05206
Sir Jonathan Kariuki <jonamkariuki@gmail.com>
2023-06-14 23:51:49 +0300

    factorial corrections

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 
 //Write a script that computes and prints a factorial
-int ans, x;
+int ans;
 function factorial (x) {
-	if (x === '') {
+	if (Number.isNan(x)) {
 		ans = 1;
 	} else {
 		ans = x * factorial(x - 1);
 	}
 	console.log(ans);
 }
+
+factorial(Number.parseInt(process.argv[2]));
