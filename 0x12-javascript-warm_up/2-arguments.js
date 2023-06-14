01a18dd7e75e1fc9191712400c995ff38892b989
Sir Jonathan Kariuki <jonamkariuki@gmail.com>
2023-06-14 23:18:55 +0300

    Q2 corrections

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 //Write a script that prints a message depending of the number of arguments passed
-if (process.argv.length == 1) {
+if (process.argv.length === 1) {
 	console.log('No argument');
-} else if (process.argv.length == 2){
+} else if (process.argv.length === 2){
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
