072b48c2ff5298587e8c6e12ef7e59a4e8d0092d
Sir Jonathan Kariuki <jonamkariuki@gmail.com>
2023-06-14 23:36:14 +0300

    Q4 corrections

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
 //Write a script that prints two arguments passed to it
-
-const arg0 = progress.argv.[2];
-const arg1 = progress.argv.[3];
-
+const arg0 = progress.argv[2];
+const arg1 = progress.argv[3];
 console.log(arg0 + ' is ' + arg1);
